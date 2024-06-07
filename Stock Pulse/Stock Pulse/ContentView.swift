be40e34ff0c2a8cf6b204073d2e1718e56a8773e
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2024-06-07 17:58:30 -0400

    Adds bottom nav bar, Empty vies for favorites and Search

---
+++ Stock Pulse/Stock Pulse/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Stock Pulse
+//
+//  Created by Ruwan Thalgahage on 2024-06-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stock Pulse/Stock Pulse/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Stock Pulse
+//
+//  Created by Ruwan Thalgahage on 2024-06-07.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
