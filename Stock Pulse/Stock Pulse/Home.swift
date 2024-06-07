993d41f0b8fb8cae0421654efb9e1ade8ba15b2c
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-06 23:25:17 -0400

    Removes unnecessary boiler plate code

---
+++ Stock Pulse/Stock Pulse/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Stock Pulse
+//
+//  Created by Abhay Limaye on 06-06-2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
--- Stock Pulse/Stock Pulse/Stock_PulseApp.swift
+++ Stock Pulse/Stock Pulse/Stock_PulseApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct Stock_PulseApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            Home()
         }
     }
 }
