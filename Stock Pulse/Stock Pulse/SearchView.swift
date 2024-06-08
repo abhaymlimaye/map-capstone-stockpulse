26442cea5fad046baa51aa98fa1b018b09374f24
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-08 01:00:58 -0400

    Adds Search Result count subtitle in Search View

--- Stock Pulse/Stock Pulse/SearchView.swift
+++ Stock Pulse/Stock Pulse/SearchView.swift
@@ -11,42 +11,52 @@ struct SearchView: View {
     @StateObject private var viewModel = SymbolSearchViewModel()
         @State private var showDetails = false
         @State private var selectedStock: SymbolSearchResult?
+        @State private var showRecordCount = false
+        @State private var showLoader = false
 
         var body: some View {
             NavigationStack {
                 VStack {
-                    Text("What are you looking for?")
-                        .font(.largeTitle)
-                        .padding()
-
                     HStack {
                         TextField("type a symbol or a name...", text: $viewModel.searchText)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .textFieldStyle(.roundedBorder)
                             .padding(.horizontal)
                             .submitLabel(.search)
                             .onSubmit {
+                                showLoader = true
                                 viewModel.search()
                             }
 
-                        Button(action: {
-                            viewModel.search()
-                        }) {
-                            Text("Search")
-                        }
-                        .padding(.trailing)
+//                        Button(action: {
+//                            showLoader = true
+//                            viewModel.search()
+//                        }) {
+//                            Text("Search")
+//                        }
+                        
                     }
-                    
-                    List(viewModel.results) { result in
-                        NavigationLink(destination: StockDetailView(ticker: result.symbol)) {
-                            VStack(alignment: .leading) {
-                                Text(result.symbol)
-                                    .font(.headline)
-                                Text(result.name)
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
+                    .padding(.vertical)
+              
+                    List {
+                        Section(showRecordCount ? "Found \(viewModel.results.count) matching" : "") {
+                            ForEach(viewModel.results) { result in
+                                NavigationLink(destination: StockDetailView(ticker: result.symbol)) {
+                                    VStack(alignment: .leading) {
+                                        Text(result.symbol)
+                                            .font(.headline)
+                                        Text(result.name)
+                                            .font(.subheadline)
+                                            .foregroundColor(.gray)
+                                    }
+                                }
+                                .onAppear() {
+                                    showLoader = false
+                                    showRecordCount = true
+                                }
                             }
                         }
                     }
+                    .navigationTitle("Let's find")
                 }
             }
         }
