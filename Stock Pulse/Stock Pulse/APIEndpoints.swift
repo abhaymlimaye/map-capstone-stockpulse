db3ef03889d2e40837ed46928658b9ed790c4908
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-08 15:18:06 -0400

    Adds Twelvedata Logo Endpoint

--- Stock Pulse/Stock Pulse/APIEndpoints.swift
+++ Stock Pulse/Stock Pulse/APIEndpoints.swift
@@ -23,23 +23,25 @@ struct APIEndpoints {
         "DDEZ_x3FduU8Q57rDAdo4zPBoSsIri2P",
         "xClmupp2psdxMR4GX3FmFxuWhrfIR4VO"
     ]
+    private static let apiKeysTwelvedata = [
+        "7ea73364a0ff42b097cf4fc044d4bbfb"
+    ]
     
     private static let alphavantageBaseUrl = "https://www.alphavantage.co/query"
     private static let polygonBaseUrl = "https://api.polygon.io/v3"
     private static let polygonTickerEndpoint = "/reference/tickers"
+    private static let twelvedataBaseUrl = "https://api.twelvedata.com"
+    private static let twelvedataLogoEndpoint = "/logo"
 
-    private static func getRandomAlphaVantageApiKey() -> String {
-        return apiKeysPolygon.randomElement() ?? apiKeyAlphaVantage[0]
-    }
-    private static func getRandomPolygonApiKey() -> String {
-        return apiKeysPolygon.randomElement() ?? apiKeysPolygon[0]
+    private static func getRandomApiKey(from keyList: [String]) -> String {
+        return keyList.randomElement() ?? keyList[0]
     }
 
     static func topMoversUrl() -> URL? {
         var components = URLComponents(string: alphavantageBaseUrl)
         components?.queryItems = [
             URLQueryItem(name: "function", value: "TOP_GAINERS_LOSERS"),
-            URLQueryItem(name: "apikey", value: getRandomAlphaVantageApiKey())
+            URLQueryItem(name: "apikey", value: getRandomApiKey(from: apiKeyAlphaVantage))
         ]
         return components?.url
     }
@@ -47,13 +49,13 @@ struct APIEndpoints {
     static func polygonUrl(for symbol: String) -> URL? {
         var components = URLComponents(string: "\(polygonBaseUrl)\(polygonTickerEndpoint)/\(symbol)")
         components?.queryItems = [
-            URLQueryItem(name: "apiKey", value: getRandomPolygonApiKey())
+            URLQueryItem(name: "apiKey", value: getRandomApiKey(from: apiKeysPolygon))
         ]
         return components?.url
     }
     
     static func appendPolygonApiKey(to url: String) -> String {
-        let apiKey = getRandomPolygonApiKey()
+        let apiKey = getRandomApiKey(from: apiKeysPolygon)
         return "\(url)?apiKey=\(apiKey)"
     }
     
@@ -61,8 +63,17 @@ struct APIEndpoints {
         var components = URLComponents(string: alphavantageBaseUrl)
         components?.queryItems = [
             URLQueryItem(name: "function", value: "SYMBOL_SEARCH"),
-            URLQueryItem(name: "apikey", value: getRandomAlphaVantageApiKey()),
+            URLQueryItem(name: "apikey", value: getRandomApiKey(from: apiKeyAlphaVantage)),
             URLQueryItem(name: "keywords", value: keywords)
+        ]
+        return components?.url
+    }
+    
+    static func logoUrl(symbol: String) -> URL? {
+        var components = URLComponents(string: "\(twelvedataBaseUrl)\(twelvedataLogoEndpoint)")
+        components?.queryItems = [
+            URLQueryItem(name: "symbol", value: symbol),
+            URLQueryItem(name: "apikey", value: getRandomApiKey(from: apiKeysTwelvedata))
         ]
         return components?.url
     }
