1837be196b78dc169f99df62e9625c4cf1cbfc8f
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-08 15:20:53 -0400

    Moves fetchStockDetail() API call to ViewModel File

--- Stock Pulse/Stock Pulse/StockDetailView.swift
+++ Stock Pulse/Stock Pulse/StockDetailView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct StockDetailView: View {
     let ticker: String
-    @State private var stockDetail: StockDetail? = nil
+    @StateObject private var viewModel = StockDetailViewModel()
 
     var body: some View {
         VStack {
-            if let stockDetail = stockDetail {
+            if let stockDetail = viewModel.stockDetail {
                 Text(stockDetail.name ?? ticker)
                     .font(.largeTitle)
                     .padding()
@@ -42,32 +42,12 @@ struct StockDetailView: View {
             } else {
                 ProgressView()
                     .onAppear {
-                        fetchStockDetail()
+                        viewModel.fetchStockDetail(ticker: ticker)
                     }
             }
         }
     }
 
-    func fetchStockDetail() {
-        guard let url = APIEndpoints.polygonUrl(for: ticker) else { return }
-        
-        print("\n\nStock Detail Url: ", url)
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                do {
-                    print("\nStock Detail Raw Data: ", String(data: data, encoding: .utf8) ?? "NA")
-                    let detailResponse = try JSONDecoder().decode(StockDetailResponse.self, from: data)
-                    DispatchQueue.main.async {
-                        self.stockDetail = detailResponse.results
-                        print("\nStock Detail Formatted Data: ", self.stockDetail ?? "NA")
-                        print("\nLogo Url: ", APIEndpoints.appendPolygonApiKey(to: self.stockDetail?.branding?.logoURL ?? "NA-"))
-                    }
-                } catch {
-                    print("Error decoding stock detail: \(error)")
-                }
-            }
-        }.resume()
-    }
+    
 }
 
