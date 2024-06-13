3ccb0f3baa9b82cd38f55d3fc2a74d080861b7e5
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-12 23:32:07 -0400

    #9 Adds the View for Favourites List

--- Stock Pulse/Stock Pulse/FavoritesView.swift
+++ Stock Pulse/Stock Pulse/FavoritesView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    @ObservedObject private var viewModel = FavoritesViewModel.shared
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            NavigationStack {
+                VStack {
+                    
+                    List {
+                        Section("You have \(viewModel.favorites.count) gem(s)") {
+                            ForEach(viewModel.favorites) { stock in
+                                NavigationLink(destination: StockDetailView(ticker: stock.ticker)) {
+                                    HStack {
+                                        Text(stock.ticker).font(.headline)
+                                        Spacer()
+                                        Text(stock.name).font(.subheadline).foregroundColor(.secondary).multilineTextAlignment(.trailing)
+                                    }
+                                }//navlink
+                            }
+                            .onDelete(perform: deleteFavorite)
+                            .onMove(perform: moveFavorite)
+                        }//section
+                    }//list
+                }
+                .navigationTitle("The Beloved")
+                .navigationBarItems(leading: Text("Your very own Superstars"), trailing: EditButton()) /*trailing: Image(systemName: "star.square.on.square"))*/
+            }
+        }
+    
     }
 }
 
