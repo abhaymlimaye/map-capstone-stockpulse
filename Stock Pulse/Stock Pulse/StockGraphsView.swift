0020496b3ee2981bf46c8dec25ff915ac1134965
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-06-14 21:07:42 -0400

    #6 Creates a View for Charts

--- Stock Pulse/Stock Pulse/StockGraphsView.swift
+++ Stock Pulse/Stock Pulse/StockGraphsView.swift
@@ -6,13 +6,116 @@
 //
 
 import SwiftUI
+import Charts
 
 struct StockGraphsView: View {
+    var stockDetail: StockDetail
+    @StateObject private var viewModelForLatest = TimeSeriesViewModel()
+    
+    @State private var selectedIntervalForLatest = 0
+    let intervalListForLatest = ["1min", "5min", "15min", "30min", "45min", "1h"]
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack() {
+                Text("Price Trends (\(stockDetail.currencyName?.uppercased() ?? ""))")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).fontWeight(.bold)
+                    .padding(.top)
+
+                
+                HStack(alignment: .center){
+                    Text("Most Recent").font(.title2).foregroundStyle(.secondary)
+                    
+                    Spacer()
+                    
+                    Button("", systemImage: "arrow.clockwise", action: {
+                        fetchLatestData()
+                    })
+                    
+                    Picker("Interval", selection: $selectedIntervalForLatest) {
+                        ForEach(intervalListForLatest.indices, id: \.self) { index in
+                            Text(intervalListForLatest[index]).tag(index)
+                        }
+                    }
+                    .onChange(of: selectedIntervalForLatest) {
+                        fetchLatestData()
+                    } //picker
+                }
+                .padding(.horizontal)
+                
+                LineChart(data: viewModelForLatest.timeSeriesValues ?? [])
+                    .frame(height: 300)
+            } //vstack
+            .onAppear {
+                fetchLatestData()
+            }
+        } //scrollview
+    }
+    
+    private func fetchLatestData() {
+        viewModelForLatest.fetchTimeSeries(symbol: stockDetail.ticker ?? "", interval: intervalListForLatest[selectedIntervalForLatest])
     }
 }
 
-#Preview {
-    StockGraphsView()
+struct LineChart: View {
+    var data: [ConvertedTimeSeriesValue]
+    @State private var selectedPriceMetric = 0
+    
+    private var selectedKeyPath: KeyPath<ConvertedTimeSeriesValue, Double> {
+        switch selectedPriceMetric {
+            case 0: return \.open
+            case 1: return \.close
+            case 2: return \.high
+            case 3: return \.low
+            default: return \.open
+        }
+    }
+    
+    private var selectedColor: Color {
+        switch selectedPriceMetric {
+            case 0: return .purple
+            case 1: return .cyan
+            case 2: return .green
+            case 3: return .red
+            default: return .purple
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Picker("Price Metric", selection: $selectedPriceMetric) {
+                Text("Open").tag(0)
+                Text("Close").tag(1)
+                Text("High").tag(2)
+                Text("Low").tag(3)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.bottom)
+            
+            Chart(data) { item in
+                LineMark(
+                    x: .value("Time", item.datetime ),
+                    y: .value("Price", item[keyPath: selectedKeyPath] )
+                )
+                .symbol(.circle)
+                .foregroundStyle(Color(selectedColor))
+            }
+            .chartXAxis {
+                AxisMarks(values: data.map { $0.datetime }) { _ in
+                    AxisGridLine(centered: false)
+                    AxisValueLabel(centered: false, collisionResolution: .greedy, orientation: .verticalReversed)
+                }
+                
+            }
+            .chartYAxis{
+                AxisMarks(values: data.map{ item in item[keyPath: selectedKeyPath] }) { _ in
+                    AxisGridLine(centered: true)
+                    AxisValueLabel(centered: false, collisionResolution: .greedy)
+                }
+            }
+            .chartYScale(type: .symmetricLog)
+        } //vstack
+        .padding()
+    }
 }
