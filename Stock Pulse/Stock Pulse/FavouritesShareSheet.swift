df3c69887ca36ce58a58436f6a1b1f9fa07d1d0c
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-07-20 15:42:37 -0400

    closes #11

--- Stock Pulse/Stock Pulse/FavouritesShareSheet.swift
+++ Stock Pulse/Stock Pulse/FavouritesShareSheet.swift
@@ -14,6 +14,5 @@ struct FavouritesShareSheet: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIActivityViewController {
         return UIActivityViewController(activityItems: items, applicationActivities: nil)
     }
-    
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
 }
