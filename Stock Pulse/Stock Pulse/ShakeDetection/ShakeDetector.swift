da494705c8b99fed3cbd97814f00c0d44bd7d10c
abhaymlimaye <32418776+abhaymlimaye@users.noreply.github.com>
2024-07-21 22:17:42 -0400

    #12 Adds a ShakeDetector Logic

---
+++ Stock Pulse/Stock Pulse/ShakeDetection/ShakeDetector.swift
@@ -0,0 +1,52 @@
+//
+//  ShakeDetector.swift
+//  Stock Pulse
+//
+//  Created by Abhay Limaye on 21-07-2024.
+//
+
+import SwiftUI
+import Combine
+import UIKit
+
+struct ShakeDetector: ViewModifier {
+    @State private var showRecommendationSheet = false
+    @State private var shakeNotificationCancellable: AnyCancellable?
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                shakeNotificationCancellable = NotificationCenter.default.publisher(for: .deviceDidShakeNotification)
+                    .sink { _ in
+                        showRecommendationSheet = true
+                    }
+            }
+            .onDisappear {
+                shakeNotificationCancellable?.cancel()
+            }
+            .sheet(isPresented: $showRecommendationSheet) {
+                RecommendationSheet()
+            }
+    }
+}
+
+extension View {
+    func onShake(showRecommendationSheet: Binding<Bool>) -> some View {
+        self.modifier(ShakeDetector())
+    }
+}
+
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: .deviceDidShakeNotification, object: nil)
+        }
+    }
+}
+
+extension Notification.Name {
+    static let deviceDidShakeNotification = Notification.Name("deviceDidShakeNotification")
+}
+
+
